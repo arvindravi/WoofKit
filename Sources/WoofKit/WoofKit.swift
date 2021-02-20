611c375043b5bfefcfae347ac2b8b62944d0ac64
Arvind Ravi <arvind.ravi@icloud.com>
2021-02-20 11:34:33 +0000

    Refactor Request

--- Sources/WoofKit/WoofKit.swift
+++ Sources/WoofKit/WoofKit.swift
@@ -27,7 +27,7 @@ public class WoofKit {
         
         var url: URL {
             switch self {
-            case .list: return BreedsEndpoint.baseURL.appendingPathComponent("list/all")
+            case .list: return BreedsEndpoint.baseURL.appendingPathComponent("list/all").absoluteURL
             case .images(let breed): return BreedsEndpoint.baseURL.appendingPathComponent("/breed/\(breed.name)/images")
             }
         }
@@ -35,37 +35,40 @@ public class WoofKit {
     
     public enum Error: LocalizedError {
         case invalidResponse
-        case failedToDecodedData
+        case failedToDecodeData
         
         public var errorDescription: String? {
             switch self {
             case .invalidResponse: return "Error Fetching Woof Data: Invalid Response"
-            case .failedToDecodedData: return "Failed to decode data"
+            case .failedToDecodeData: return "Failed to decode data"
             }
         }
     }
     
     public func fetchBreeds(result: @escaping BreedsListResult) {
-        URLSession.shared.dataTask(with: BreedsEndpoint.list.url.request) { (data, _, _) in
+        URLSession.shared.dataTask(with: BreedsEndpoint.list.url) { (data, response, error) in
+            guard let response = response as? HTTPURLResponse,
+                  response.statusCode == 200 else {
+                result(.failure(.invalidResponse))
+                print(error)
+                return
+            }
+            print(data)
             self.handleData(data: data, result: result)
         }.resume()
     }
     
     private func handleData(data: Data?, result: @escaping BreedsListResult) {
         guard let data = data else {
-            result(.failure(.failedToDecodedData))
+            result(.failure(.failedToDecodeData))
             return
         }
         
         guard let decoded = try? decoder.decode(BreedsRawResponse.self, from: data) else {
-            result(.failure(.failedToDecodedData))
+            result(.failure(.failedToDecodeData))
             return
         }
         
         result(.success(decoded.message))
     }
-}
-
-private extension URL {
-    var request: URLRequest { URLRequest(url: self) }
 }
